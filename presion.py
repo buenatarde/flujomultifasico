8c425512e7f84dcadd5598490266d55b65dd6ce8
buenatarde <omarpatinosierra@gmail.com>
2021-08-25 21:06:23 -0500

    Empece pidiendo al usurio si quiere la presion en cabeza o en fondo de pozo

---
+++ presion.py
@@ -0,0 +1,18 @@
+def run():
+    print("Este codigo realizara los calculos necesarios para determinar la presion en cabeza o en fondo dependiendo lo que usted requiera")
+    print("\nSi usted quiere la presion a la cabeza coloque 1, pero si quiere la presion en fondo coloque 2")
+    
+    cabezaofondo = input("\nDeme su eleccion [1] o [2]:\t")
+    assert cabezaofondo.isnumeric() and int(cabezaofondo) >= 1 and int(cabezaofondo) <= 2, "\nFavor de Ingresar un numero que sea 0 o 1"
+    
+    cabezaofondo = int(cabezaofondo)
+    
+    if cabezaofondo == 1:
+        print("\nUsted eligio calcular el valor de la presion en cabeza de pozo")
+    else:
+        print("\nUsted eligio calcular el valor de la presion en fondo de pozo")
+    
+    
+
+if __name__ == "__main__":
+    run ()
