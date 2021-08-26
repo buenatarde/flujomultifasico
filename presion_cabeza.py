13ff34bdb4e52a78a4ca54c4a91f73c3553164c2
buenatarde <omarpatinosierra@gmail.com>
2021-08-25 20:18:59 -0500

    Cree el archivo de presion a la cabeza el cual sera el main para este proyecto

---
+++ presion_cabeza.py
@@ -0,0 +1,6 @@
+def run():
+    print("Este codigo realizara los calculos necesarios para determinar la presion en cabeza o en fondo dependiendo lo que usted requiera")
+    print("Si usteded quiere que que la presion a la cabeza coloque 0, pero si quiere la presion en fondo coloque 1")
+
+if __name__ == "__main__":
+    run ()
