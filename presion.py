409c860abbcdbd16d22c79233ed5ec9f86b46da2
buenatarde <omarpatinosierra@gmail.com>
2021-08-26 19:04:12 -0500

    Agregue los asserts para evitar datos erroneos

--- presion.py
+++ presion.py
@@ -1,4 +1,6 @@
 def run():
+    #!/usr/bin/python
+    # -*- coding: utf-8 -*-
     print("Este codigo realizara los calculos necesarios para determinar la presion en cabeza o en fondo dependiendo lo que usted requiera")
     print("\nSi usted quiere la presion a la cabeza coloque 1, pero si quiere la presion en fondo coloque 2")
     
@@ -12,9 +14,27 @@ def run():
     else:
         print("\nUsted eligio calcular el valor de la presion en fondo de pozo")
     
-    Profundidad = float(input("\n¿Cual es la profundidad en pies total del pozo?:\t"))
-    Ti = float(input("\n¿Cual es la temperatura inical en Farenheit del pozo?:\t"))
-    PresionInicial = float(input("\n¿Cual es la presion en psia del pozo?:\t"))
+    Profundidad = input("\n¿Cual es la profundidad en pies total del pozo?:\t")
+    assert Profundidad.isnumeric(), "\n\nPor favor ingresa valores numericos"
+    Profundidad=float(Profundidad)
+
+    Ti = input("\n¿Cual es la temperatura inical en Farenheit del pozo?:\t")
+    assert Ti.isnumeric(), "\n\nPor favor ingresa valores numericos"
+    Ti=float(Ti)
+
+    PresionInicial = input("\n¿Cual es la presion en psia del pozo?:\t")
+    assert PresionInicial.isnumeric(), "\n\nPor favor ingresa valores numericos"
+    PresionInicial=float(PresionInicial)
+
+    Qo = input("\n¿Cual es el gasto de aceite en barriles por dia del pozo?:\t")
+    assert Qo.isnumeric(), "\n\nPor favor ingresa valores numericos"
+    Qo=float(Qo)
+
+    ygt = input("\n¿Cual es la densidad relativa del gas total (los valores van entre 0 y 1)?:\t")
+    assert ygt.isnumeric() and ygt >=0 and ygt <= 1, "\n\nPor favor ingresa valores numericos o valores dentro del rango"
+    ygt=float(ygt)
+    
+    
 
 def propiedades_vazquez():
     pass
