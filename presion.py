78490c90a6a9c17bf6feacaface48d0bc23cadf2
buenatarde <omarpatinosierra@gmail.com>
2021-09-11 14:14:08 -0500

    Se pusieron las restricciones para que solo se puedan ingresar valores numericos into o float

--- presion.py
+++ presion.py
@@ -1,6 +1,4 @@
 def run():
-    #!/usr/bin/python
-    # -*- coding: utf-8 -*-
     print("Este codigo realizara los calculos necesarios para determinar la presion en cabeza o en fondo dependiendo lo que usted requiera")
     print("\nSi usted quiere la presion a la cabeza coloque 1, pero si quiere la presion en fondo coloque 2")
     
@@ -16,26 +14,27 @@ def run():
     
     print("\nEn las siguientes peticiones por favor llenarla con numeros enteros o decimales\n")
 
-    Profundidad = float(input("\n¿Cual es la profundidad en pies total del pozo?:\t"))
+    Profundidad = input("\n¿Cual es la profundidad en pies total del pozo?:\t")
+    assert Profundidad.replace(".","",1).isdigit(), "\n\nPor favor ingresa valores numericos"
     Profundidad = float(Profundidad)
 
     Ti = input("\n¿Cual es la temperatura inical en Farenheit del pozo?:\t")
-    assert Ti.isdecimal(), "\n\nPor favor ingresa valores numericos"
+    assert Ti.replace(".","",1).isdigit(), "\n\nPor favor ingresa valores numericos"
     Ti = float(Ti)
 
     PresionInicial = input("\n¿Cual es la presion en psia del pozo?:\t")
-    assert PresionInicial.isnumeric(), "\n\nPor favor ingresa valores numericos"
+    assert PresionInicial.replace(".","",1).isdigit(), "\n\nPor favor ingresa valores numericos"
     PresionInicial = float(PresionInicial)
 
     Qo = input("\n¿Cual es el gasto de aceite en barriles por dia del pozo?:\t")
-    assert Qo.isnumeric(), "\n\nPor favor ingresa valores numericos"
+    assert Qo.replace(".","",1).isdigit(), "\n\nPor favor ingresa valores numericos"
     Qo = float(Qo)
 
     ygt = input("\n¿Cual es la densidad relativa del gas total (los valores van entre 0 y 1)?:\t")
-    assert ygt.isnumeric() and ygt >= float(0.0) and ygt <= float(1.0), "\n\nPor favor ingresa valores numericos o valores dentro del rango"
+    assert ygt.replace(".","",1).isdigit(), "\n\nPor favor ingresa valores numericos o valores dentro del rango"
     ygt = float(ygt)
-    
-    
+    assert ygt >= float(0.0) and ygt <= float(1.0), "\n\nPor favor ingresa valores dentro del rango"
+       
 
 def propiedades_vazquez():
     pass
